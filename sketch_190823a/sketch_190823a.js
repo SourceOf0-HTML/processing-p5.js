5098392b1b98736bc2c742cb6569841876c96f55
Source of 0 <SourceOf0@gmail.com>
2019-08-24 12:28:48 +0900

    amend 190823a

--- sketch_190823a/sketch_190823a.js
+++ sketch_190823a/sketch_190823a.js
@@ -1,20 +1,18 @@
-
+/*
 setup=_=>{createCanvas(S=500,S);P=PI;i=push;o=pop;t=translate;noStroke()}
 draw=_=>{s=30;i();colorMode(HSB);t(p=S/2,p/=2);a=atan2(mouseY-p,mouseX-p*2)+P;m(p);o()}
 m=(l)=>{rotate(a+P/2);fill(a/P*180,s+=5,100);rect(w=l*.1,0,-w*2,l);if(l>1)i(),t(0,l/2),m(l*.8,a),o()}
 /**/
-
-/*
 function setup() {
   createCanvas(S=500, S);
+  noStroke();
 }
 
 function draw() {
-  background(0);
   p = S/2;
   P = PI;
   i = 30;
-  angle = atan2( mouseY - p, mouseX - p ) + P;
+  angle = atan2( mouseY - p/2, mouseX - p ) + P;
   
   push();
   colorMode(HSB);
@@ -23,15 +21,15 @@ function draw() {
   pop();
 }
 
-function m( l, a ) {
-  w = l*.1;
-  rotate( a+P/2 );
-  fill( a/P*180, i+=5, 100 );
-  rect( -w, 0, w*2, l );
-  if( l>1 ){
+function m( l ) {
+  w = l * 0.1;
+  rotate( angle + P/2 );
+  fill( angle / P * 180, i += 5, 100 );
+  rect( -w, 0, w * 2, l );
+  if( l > 1 ){
     push();
-    translate( 0, l*.5 );
-    m( l*.9, a );
+    translate( 0, l/2 );
+    m( l * 0.8, angle );
     pop();
   }
 }
