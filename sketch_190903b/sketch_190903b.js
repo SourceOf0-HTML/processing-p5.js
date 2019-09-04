be6e89797046d788d6aa9b9d629338f2f8bf01c1
Source of 0 <SourceOf0@gmail.com>
2019-09-04 18:35:38 +0900

    add original movie

--- sketch_190903b/sketch_190903b.js
+++ sketch_190903b/sketch_190903b.js
@@ -3,11 +3,11 @@ setup=_=>{createCanvas(S=500,S);H=S/2;o=new p5.SinOsc();e=new p5.Envelope();e.se
 draw=_=>{clear();w=f.waveform();l=w.length;for(i=0;i<l;)r=i/l,stroke(r*80+170,100,80),point(r*S,w[++i]*H/4+H)}
 s=_=>{setInterval(_=>{!t?o.start():0;o.freq(midiToFreq([60,62,64,65,67,69,71,72][n]));e.play(o,0,.1);n=++n%8;++t},1e3)}
 /**/
-
+/*
 setup=_=>{S=99;H=S/2;o=new p5.SinOsc();e=new p5.Envelope();e.setADSR(.01,.5,.1);f=new p5.FFT();t=n=0;o.start()}
 draw=_=>{t++%60==0?(o.freq(midiToFreq(60+[0,4,7,12][n++])),e.play(o,0,1),n%=4):0;clear();w=f.waveform();i=0;w.some(T=>{point(i++/w.length*S,T*H/4+H)})}
 /**/
-/*
+
 function setup() {
   createCanvas(S=500, S);
   H = S/2;
