9445483e573ec3f068687dbdefd5376f6013206a
Source of 0 <SourceOf0@gmail.com>
2019-12-30 16:19:11 +0900

    amend 190825a code simplify

--- sketch_190825a/sketch_190825a.js
+++ sketch_190825a/sketch_190825a.js
@@ -1,8 +1,17 @@
+/*
+setup=_=>{R=random;V=createVector;createCanvas(S=500,S);i(D=[]);noStroke()}
+draw=_=>{background(0),m(D)}
+i=(d,l=10,T,n=0)=>{c=T?T.c+1:0;while(n<80-c*30){d[n++]=t={c,p:T?T.p.copy():V(),v:V(R(l*.9)).rotate(R(TAU))};t.l=t.v.mag()}}
+m=d=>d.some(T=>{l=T.v.mag();l>2?(T.p.add(T.v.mult(.95)),circle(T.p.x+S/2,T.p.y+S/2,l/2)):(T.d?m(T.d):(T.c<2?i(T.d=[],T.l,T):0))})
+/**/
 
-setup=_=>{r=random;v=createVector;createCanvas(S=500,S);d=[];i(d,10);noStroke()}
-draw=_=>{background(0);translate(H=S/2,H);m(d)}
-i=(D,L,T=0)=>{c=(T)?T.c+1:0;for(n=0;n<80-c*30;){D[n++]=t={c:c,p:(T)?T.p.copy():v(),v:v(r(L*.9)).rotate(r(PI*2))};t.l=t.v.mag()}}
-m=(D)=>{D.some(T=>{l=T.v.mag();(l>2)?(T.p.add(T.v.mult(.95)),circle(T.p.x,T.p.y,l/2)):((T.d)?(m(T.d)):((T.c<2)?(i(T.d=[],T.l,T)):0))});}
+S=500
+L=9
+setup=_=>i(D=[])
+draw=_=>m(D,createCanvas(S,S))
+r=_=>random(-L,L)
+i=(d,c=0,x=S/2,y=S/2)=>{for(n=0;n<30-c*14;++n)d[n]={c,x,y,l:mag(u=r(),v=r()),u,v}}
+m=d=>d.some(T=>(l=mag(T.u,T.v))>1?circle(x=T.x+=T.u*=.95,y=T.y+=T.v*=.95,l):T.d?m(T.d):T.c<2?i(T.d=[],T.c+1,x,y,L=T.l):0)
 /**/
 
 /*
