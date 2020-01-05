71f369b60121067e9daa9b0a974388cde7a6c390
Source of 0 <SourceOf0@gmail.com>
2020-01-05 19:08:38 +0900

    amend 190829a simplify code and movie

--- sketch_190829a/sketch_190829a.js
+++ sketch_190829a/sketch_190829a.js
@@ -1,9 +1,9 @@
-/*
-setup=_=>{N=noise;createCanvas(S=500,S);H=S/2;T=C=0;colorMode(HSB)}
-draw=_=>{background(0);push();blendMode(ADD);for(j=0;j<24;j+=3)m(T+j,C+j);pop();T+=N(C)*.001;C+=.005;}
-m=(t,c)=>{x=y=v=0;for(i=0;i<1;i+=1/H)stroke((i+c)*4%360,70,s=(1-i)*20),strokeWeight(s),v+=noise(i-t)/2,x+=(N(i-t-c)-.5)*v,y+=(N(i-t-c+S)-.5)*v,point(H+x,H+y)}
-/**/
 
+T=C=0
+draw=_=>{createCanvas(S=500,S),N=noise,H=S/2,colorMode(HSB),background(j=0);blendMode(ADD);for(;j<24;j+=3)m(T+j,C+j);T+=N(C+=.005)*.001}
+m=(t,c)=>{for(x=y=v=i=0;i<1;i+=1/H)fill((i+c)*4%360,70,s=10-i*10),v+=N(a=i-t),x+=(N(a-c)-.5)*v,y+=(N(a-c+S)-.5)*v,circle(H+x,H+y,s)}
+/**/
+/*
 setup=_=>{N=noise;createCanvas(S=500,S);H=S/2;T=C=0}
 draw=_=>{clear();j=24;while(j-=3)m(T+j,C+j);T+=N(C+=.005)*.01}
 m=(t,c)=>{for(x=y=i=v=0;i<1;i+=1/H)stroke(0,M=255,M,i*M),strokeWeight((1-i)*20),v+=N(i-t),x+=(N(i-t-c)-.5)*v,y+=(N(i-t-c+S)-.5)*v,point(H+x,H+y)}
